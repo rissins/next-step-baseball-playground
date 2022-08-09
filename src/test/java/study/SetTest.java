dc8fc0212121117996780bc2351b4eb33c6d85d3
rissins <tnalsqqq@naver.com>
2022-08-10 03:49:17 +0900

    test: find contains with @ParameterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,7 +25,18 @@ public class SetTest {
      * Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
      */
     @Test
-    void getSetSize() {
+    void size() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    /**
+     * Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+     * 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+     * JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+     */
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        Assertions.assertThat(numbers).contains(input);
     }
 }

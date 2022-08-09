6f1ffa886a97c618321467a88f4df0944f3cb792
rissins <tnalsqqq@naver.com>
2022-08-10 03:16:49 +0900

    test: split

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,32 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /**
+     * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+     */
+    @Test
+    void split_1() {
+        //given
+        String string = "1,2";
+        //when
+        String[] split = string.split(",");
+        //then
+        Assertions.assertThat(split).contains("1");
+        Assertions.assertThat(split).contains("2");
+    }
+
+    /**
+     * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+     */
+    @Test
+    void split_2() {
+        //given
+        String string = "1";
+        //when
+        String[] split = string.split(",");
+        //then
+        Assertions.assertThat(split).containsOnly("1");
     }
 }

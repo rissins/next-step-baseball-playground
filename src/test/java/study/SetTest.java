05bd60e4a139ea4f0e89689bb4a1920923f84acd
rissins <tnalsqqq@naver.com>
2022-08-10 03:57:24 +0900

    test: getContains with @CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -38,5 +39,15 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input) {
         Assertions.assertThat(numbers).contains(input);
+    }
+
+    /**
+     * 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+     * 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
+     */
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsWithFalseCase(int input, boolean excepted) {
+        Assertions.assertThat(numbers.contains(input)).isEqualTo(excepted);
     }
 }

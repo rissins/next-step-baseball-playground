5dcae07b786438a3780e1553561cbb037fd9ec13
rissins <tnalsqqq@naver.com>
2022-08-10 03:40:15 +0900

    test: charAt, exception

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -50,5 +51,26 @@ public class StringTest {
         String substring = string.substring(1, string.length() - 1);
         //then
         Assertions.assertThat(substring).isEqualTo("1,2");
+    }
+
+    /**
+     * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+     * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+     * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+     */
+    @Test
+    void findStringByChaAt() {
+        //given
+        String string = "abc";
+        //when
+        char getZeroIndexByCharAt = string.charAt(0);
+        char getOneIndexByCharAt = string.charAt(1);
+        char getTwoIndexByCharAt = string.charAt(2);
+        //then
+        Assertions.assertThat(getZeroIndexByCharAt).isEqualTo('a');
+        Assertions.assertThat(getOneIndexByCharAt).isEqualTo('b');
+        Assertions.assertThat(getTwoIndexByCharAt).isEqualTo('c');
+        Assertions.assertThatThrownBy(() -> string.charAt(string.length()))
+                .isInstanceOf(IndexOutOfBoundsException.class);
     }
 }

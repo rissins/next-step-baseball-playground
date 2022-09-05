9f65b7c3f36b00064a4f6d90b2db84ed40a6090f
rissins <tnalsqqq@naver.com>
2022-09-05 22:26:47 +0900

    rename: Computer -> GenerateRandomNum

--- src/main/java/tdd/baseball/domain/GenerateRandomNum.java
+++ src/main/java/tdd/baseball/domain/GenerateRandomNum.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Computer {
+public class GenerateRandomNum {
 
     public int randomMake(){
         Random random = new Random();

4b23147c369bde2fa86b4b83db68284158229af5
rissins <tnalsqqq@naver.com>
2022-09-05 22:27:15 +0900

    feat: 실행 메인메서드 작성

--- src/main/java/tdd/baseball/Main.java
+++ src/main/java/tdd/baseball/Main.java
@@ -1,14 +1,28 @@
 package tdd.baseball;
 
+import tdd.baseball.domain.GenerateRandomNum;
 import tdd.baseball.request.Input;
-import tdd.baseball.request.View;
+import tdd.baseball.request.PlayAgain;
+import tdd.baseball.service.Judge;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        GenerateRandomNum randomNum = new GenerateRandomNum();
         Input input = new Input();
-        View view = new View();
+        Judge judge = new Judge();
+        PlayAgain playagain = new PlayAgain();
+        boolean again = true;
 
-        view.requestNumber();
-        input.inputNumber();
+        while (again){
+            List<Integer> computer = randomNum.create();
+            String result = "";
+            while (!result.equals("3스트라이크")){
+                result = judge.judgement(computer, input.playerNumber());
+                System.out.println(result);
+            }
+            again = playagain.playAgain();
+        }
     }
 }
